e1188ae6ad3eeb4d60fdfe23d97299967d337285
Luciano Buono <luciano.buono@saritasa.com>
2025-03-08 02:01:58 -0300

    feat: add Dockefile, change to use remote selenium

--- src/parse.py
+++ src/parse.py
@@ -1,6 +1,7 @@
 import os
 from email import message_from_file
 from email.message import Message
+from utils.logger import logger
 
 NETFLIX_LINK_START = ["https://www.netflix.com/account/update-primary", "https://www.netflix.com/account/set-primary"]
 
@@ -8,7 +9,7 @@ NETFLIX_LINK_START = ["https://www.netflix.com/account/update-primary", "https:/
 def parse_email_from_file(file_path: str):
     """Read and parse an email file (.eml or raw format)."""
     if not os.path.exists(file_path):
-        print(f"❌ File not found: {file_path}")
+        logger.info(f"❌ File not found: {file_path}")
         return
 
     # Open and parse the email
@@ -30,7 +31,7 @@ def get_netflix_link(email_data):
         if idx_start != -1:
             break
         if idx_start == -1:
-            print(
+            logger.info(
                 "Unable to parse the correct link in the Email. "
                 "Maybe the search string is not correct anymore or the Email is not a update Email?"
             )
--- src/selenium_utils/constants.py
+++
@@ -1,7 +0,0 @@
-import os
-
-PORT = int(os.getenv("PORT", 8000))
-NETFLIX_EMAIL = os.getenv("NETFLIX_EMAIL", "FILL")
-NETFLIX_PASSWORD = os.getenv("NETFLIX_PASSWORD", "FILL")
-
-test_link = 'https://www.netflix.com/account/update-primary-location?nftoken=BgjolOvcAxKkAbXjIkTxYiANxGkuLny44J0xNhg028uy5xutdsi0giQ62dVYIpKEWlxcgYempR1y6Dt2XvujuKRhElprB6qJTwRR8tfxRAE9VkcdfyKsBfcyN/LAjB2dHBC1nrc7MtnlBtlEJ/hcicNL4f1RKrxmAHOBz8GNeUmeHNiKn+j8qYrsqA155eKT8Y4lDdv8r4u32Vx5GwaJ6lVQugNfIYuZsn+4e6+kGAYiDgoMlrvzM8A6ZVr7uAAm&g=4802e319-e06c-42e2-96bf-8408a3a10d9f&lnktrk=EVO&operation=update&lkid=UPDATE_HOUSEHOLD_REQUESTED_OTP_CTA'
